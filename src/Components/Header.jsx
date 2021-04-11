856a9f424714dfc3ec69cd8eded9c4a294937d1b
hermann tamouya <hermann12@gmail.com>
2021-04-11 14:35:58 +0200

    fix the logo and the home link

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,13 +5,13 @@ function Header() {
   return (
     <Fragment>
       <Navbar expand="lg" className="container my-3">
-        <Navbar.Brand href="/" className="text-danger">
+        <Navbar.Brand href="/frontend-hermann-shop" className="text-danger">
           Logo
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/frontend-hermann-shop">Home</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
